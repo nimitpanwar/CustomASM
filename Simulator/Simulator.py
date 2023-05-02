3db12f9b7763e3589c84f7c401ce4737f69c73c6
Nimit Panwar <71833404+nejim3h@users.noreply.github.com>
2023-05-02 14:21:12 +0530

    Update Simulator.py

--- Simulator/Simulator.py
+++ Simulator/Simulator.py
@@ -1,4 +1,54 @@
-# Commit code here -
+def aand(a,b):
+    s=""
+    for i in range(min(len(a), len(b))):
+        s+='1'if int(a[i])==int(b[i])==1 else '0'
+    return s
+
+def oor(a,b):
+    s=""
+    for i in range(min(len(a), len(b))):
+        s+='0' if int(a[i])==int(b[i])==0 else '1'
+    return s
+
+def xor(a,b):
+    s=""
+    for i in range(min(len(a), len(b))):
+        s+='0' if int(a[i])!=int(b[i]) else '1'
+    return s
+
+def invert(a):
+    s=""
+    for i in range(len(a)):
+        if int(a[i])==1:
+            s+='0' 
+        else:
+            s+='1'
+    return s
+
+
+def DecToBin7(decimal_num):
+    binary_num = bin(decimal_num)[2:]
+    binary_num = '0' * (7 - len(binary_num)) + binary_num
+    return binary_num
+
+
+def DecToBin16(decimal_num):
+    binary_num = bin(decimal_num)[2:]
+    binary_num = '0' * (16 - len(binary_num)) + binary_num
+    return binary_num
+
+
+def BinToDec(binary):
+    decimal_num = 0 
+    x = 0
+    while(binary != 0):
+        dec = int(binary) % 10
+        decimal_num += dec * pow(2, x)
+        binary= int(binary)//10
+        x+= 1
+    return decimal_num
+
+
 opcodes = {
     'add':["00000","A"],
     'sub':["00001","A"],
@@ -56,12 +106,8 @@ registers = {
 	'FLAGS':'111' 
 }
 
-
-
-
 with open("sample.txt","r") as f:
     contents=f.readlines()
-
 
 def iType(a):
     return opcodeType[a[:5]]
